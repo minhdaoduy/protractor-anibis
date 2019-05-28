3af2ada70ae5f4432409ad390f4567d2b046e3c1
ddminh <daoduyminh@gmail.com>
2019-05-28 15:39:06 +0700

    no message

--- infrastructure/helpers/json.helper.js
+++ infrastructure/helpers/json.helper.js
@@ -57,6 +57,52 @@ class JsonHelper {
 
         return result['config'][configKey];
     }
+    
+    //Read data from json file and return array object
+    static readJsonArray(filePath) {
+        //Import the fs module
+        const fs = require('fs');
+
+        //Read the json file into row data
+        let rowData = fs.readFileSync(filePath);
+
+        //parse the row data in to Json format
+        let result = JSON.parse(rowData);
+
+        return JSON.parse(rowData);
+    }
+    
+    static readTestData(dataKeyName) {
+        //Import the fs module
+        const fs = require('fs');
+
+        //Read the json file into row data
+        let rowData = fs.readFileSync(strTestDataFile);
+
+        //parse the row data in to Json format
+        let result = JSON.parse(rowData);
+
+        let index = this.readConfig('testDataIndexLine')
+
+        return result['testData'][parseInt(index.toString()) - 1][dataKeyName];
+    }
+
+    static isEqual (j1, j2) {
+        let result = false;
+
+        if (Object.keys[j1].length == Object.keys(j2).length) {
+            for (let key in j1) {
+                if (j1[key] === j2[key])
+                    result = true;
+                else {
+                    result = false;
+                    break;
+                }
+            }
+        } else result = false;
+
+        return result;
+    }
 
 }
 module.exports = JsonHelper;
